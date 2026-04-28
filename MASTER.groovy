e0e179cff9be90f55fd415a5a6ce46dc383e1eb0
Eymen Doğru <muh.eymendogru@gmail.com>
2026-04-28 17:46:43 +0300

    MASTER.groovy and pipeline_steps

---
+++ MASTER.groovy
@@ -0,0 +1,41 @@
+node {
+
+    currentBuild.getChangeSets().clear()
+
+    String BUILD_TEST_STAGE = "Build & Test"
+    String SONARQUBE_STAGE = "SonarQube Check Stage"
+    String VERSION_STAGE = "Version Bump"
+    String DOCKER_STAGE = "Docker Build & Tag"
+    String GHCR_PUSH_STAGE = "GHCR Push"
+
+    // Jenkins Parameters
+    String PROJECT_ID = params.PROJECT_ID
+    println("PROJECT_ID: $PROJECT_ID")
+
+    // Paths
+    String projectsFilePath = "./projects.yml"
+    
+    def projects = readYaml(file: projectsFilePath)['projects']
+    def project = projects[PROJECT_ID]
+    String projectName = project['name']
+    String projectCodeRepoUrl = project['codeRepo']['url']
+    String projectCredentialsId = project['codeRepo']['credentialsId']
+
+
+    deleteDir()
+
+    try {
+
+        build job: 'maven-build-job', wait: true, propagate: false,
+        parameters: [
+            string(name: 'CODE_URL', value: projectCodeRepoUrl),
+            string(name: 'CREDENTIALS_ID', value: projectCredentialsId)
+        ]
+
+    } catch(Exception e) {
+
+        echo "Error occurred: ${e.message}"
+
+    }
+
+}
---
+++ pipeline_steps/buildAndTest.groovy
@@ -0,0 +1,69 @@
+node {
+
+    deleteDir()
+
+    // Build edilecek dosyalara ihtiyacım var
+    // Build edilecek ortama ve tool'lara ihtiyacım var
+    // Test için gerekli tool'lara ihtiyacım var
+    String projectCodeRepoUrl = params.CODE_URL
+    String codeRepoCredentialsId = params.CREDENTIALS_ID
+
+    String BUILD_STAGE = 'Build'
+    String TEST_STAGE = 'Test'
+ 
+
+    stage (BUILD_STAGE) {
+        
+        String buildDir = "build"
+        dir(buildDir){
+            checkout([$class: 'GitSCM'], 
+            branches: [[name: '*/main']],
+            userRemoteConfigs: [[
+                url: projectCodeRepoUrl,
+                credentialsId: codeRepoCredentialsId
+            ]])
+        }
+
+        buildMaven()
+        
+    }
+
+    stage (TEST_STAGE) {
+        String testDir = "test"
+        dir(testDir){
+            checkout([$class: 'GitSCM'], 
+            branches: [[name: '*/main']],
+            userRemoteConfigs: [[
+                url: projectCodeRepoUrl,
+                credentialsId: codeRepoCredentialsId
+            ]])
+        }
+
+        testMaven()
+    }
+}
+
+def buildMaven(){
+
+    echo 'Build maven running...'
+
+    String mavenPath = tool 'maven_3.9'
+    String mavenBuildPath = "PATH+MAVEN=$mavenPath/bin"
+
+    withEnv([mavenBuildPath]){
+        sh 'mvn clean package'
+    }
+
+}
+
+def testMaven(){
+
+    echo 'Test maven running...'
+
+    String mavenPath = tool 'maven_3.9'
+    String mavenBuildPath = "PATH+MAVEN=$mavenPath/bin"
+
+    withEnv([mavenBuildPath]){
+        sh 'mvn test'
+    }
+}
